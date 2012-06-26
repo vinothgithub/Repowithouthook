2a46b30c5459c27eaf43eceebfc2fc5f07be59a1
vinothgithub <vinothks@softcrylic.co.in>
2012-06-27 00:07:58 +0530

    final testing

--- src/PageObjects/HomePage.java
+++ src/PageObjects/HomePage.java
@@ -2,7 +2,7 @@ package PageObjects;
 
 public class HomePage {
 	public static String getAppleURL(){
-		return "http://www.apple.com/";
+		return "http://www.yahoo.com/";
 	}
 	public static String getMagentoURL(){
 		return "http://www.magento-mall.com";
