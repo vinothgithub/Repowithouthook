6892ed0df63879d23b027484a477db727a471f59
vinothgithub <vinothks@softcrylic.co.in>
2012-06-26 23:52:46 +0530

    asedasd

--- src/PageObjects/HomePage.java
+++ src/PageObjects/HomePage.java
@@ -2,7 +2,7 @@ package PageObjects;
 
 public class HomePage {
 	public static String getAppleURL(){
-		return "http://www.gmail.com/";
+		return "http://www.google.co.in/";
 	}
 	public static String getMagentoURL(){
 		return "http://www.magento-mall.com";
