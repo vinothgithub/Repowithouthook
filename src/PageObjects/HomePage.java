2836ff0e9c44fd9c4586b37e9ccc1785c2734136
vinothgithub <vinothks@softcrylic.co.in>
2012-07-19 03:37:55 +0530

    sdfdsf

--- src/PageObjects/HomePage.java
+++ src/PageObjects/HomePage.java
@@ -2,7 +2,7 @@ package PageObjects;
 
 public class HomePage {
 	public static String getAppleURL(){
-		return "http://www.google.com/";
+		return "http://www.google.co.in/";
 	}
 	public static String getMagentoURL(){
 		return "http://www.magento-mall.com";
