0f7c84b59a267f7f74250f59ca2e720c30caa1c2
vinothgithub <vinothks@softcrylic.co.in>
2012-07-23 18:24:18 +0530

    dev commit for exe to parse xml

--- src/PageObjects/HomePage.java
+++ src/PageObjects/HomePage.java
@@ -2,7 +2,7 @@ package PageObjects;
 
 public class HomePage {
 	public static String getAppleURL(){
-		return "http://www.yahoo.co.in/";
+		return "http://www.yahoo.com/";
 	}
 	public static String getMagentoURL(){
 		return "http://www.magento-mall.com";
