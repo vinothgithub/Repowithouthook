5d818843f41ce601a2279dcf7518dc082b8037e6
vinothgithub <vinothks@softcrylic.co.in>
2012-07-23 18:27:54 +0530

    i m here

--- src/PageObjects/HomePage.java
+++ src/PageObjects/HomePage.java
@@ -2,7 +2,7 @@ package PageObjects;
 
 public class HomePage {
 	public static String getAppleURL(){
-		return "http://www.yahoo.com/";
+		return "http://www.yahoo.co.in/";
 	}
 	public static String getMagentoURL(){
 		return "http://www.magento-mall.com";
