e6c815669817bc3226c4b7c8f2295cd11cb44e5e
vinothgithub <vinothks@softcrylic.co.in>
2012-07-25 17:02:52 +0530

    check for build and replace

--- src/PageObjects/HomePage.java
+++ src/PageObjects/HomePage.java
@@ -2,7 +2,7 @@ package PageObjects;
 
 public class HomePage {
 	public static String getAppleURL(){
-		return "http://www.yahoo.co.in/";
+		return "http://www.google.in/";
 	}
 	public static String getMagentoURL(){
 		return "http://www.magento-mall.com";
